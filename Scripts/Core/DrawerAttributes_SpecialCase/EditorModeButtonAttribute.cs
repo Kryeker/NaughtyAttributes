1057684cb8ab93b92226a21c3a70aed019c7bd8b
Artem Moldovanenko <kryeker3@gmail.com>
2024-07-29 08:22:31 +0300

    Add EditorModeButton and PlayModeButton

---
+++ Scripts/Core/DrawerAttributes_SpecialCase/EditorModeButtonAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NaughtyAttributes
+{
+    public class EditorModeButtonAttribute : ButtonAttribute
+    {
+        public EditorModeButtonAttribute(string text = null) : base(text, EButtonEnableMode.Editor)
+        {
+
+        }
+    }
+}
---
+++ Scripts/Core/DrawerAttributes_SpecialCase/PlayModeButtonAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NaughtyAttributes
+{
+    public class PlayModeButtonAttribute : ButtonAttribute
+    {
+        public PlayModeButtonAttribute(string text = null) : base(text, EButtonEnableMode.Playmode)
+        {
+
+        }
+    }
+}
